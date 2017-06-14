9ee14fd6caeead9b7623c9fad544ce100a3fa416
151610202-smkassalaambandung <151610202@smkassalaambandung.sch.id>
2017-06-14 09:23:23 +0700

    projek baru

--- app/Http/Controllers/SayaController.php
+++ app/Http/Controllers/SayaController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class SayaController extends Controller
 {
     //
+public function __construct()
+{
+	$this->middleware('auth');
+}
+
+
     public function saya()
     {
     	return "selamat datang";
